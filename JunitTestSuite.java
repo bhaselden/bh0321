63ba9c68ca02dcbe201775675d488aab45328900
bhaselden <47121221+bhaselden@users.noreply.github.com>
2021-03-27 22:15:41 -0500

    Add files via upload

---
+++ JunitTestSuite.java
@@ -0,0 +1,13 @@
+package toolRentalApplication;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   tests.class
+})
+public class JunitTestSuite {
+
+}
